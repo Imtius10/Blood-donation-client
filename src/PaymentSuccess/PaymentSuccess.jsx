e428b7fb1697e12055af304cb9478db2b17f3a63
Your Name <h.imtius10@gmail.com>
2025-12-20 04:48:05 +0600

    payment success page

--- src/PaymentSuccess/PaymentSuccess.jsx
+++ src/PaymentSuccess/PaymentSuccess.jsx
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSearchParams } from 'react-router';
+import useAxios from '../Hooks/UseAxios';
 
 const PaymentSuccess = () => {
+
+    const [searchParams] = useSearchParams();
+    const sessionId = searchParams.get('session_id');
+    const axiosInstance = useAxios();
+    useEffect(() => {
+        axiosInstance.post(`/success-payment?session_id=${sessionId}`)
+    },[axiosInstance, sessionId])
     return (
         <div>
             PaymentSuccess
